437cd238baa9fc6024e996b8ac3cf54ca3965fba
Gean <000cuiquesuum000@gmail.com>
2022-03-29 14:03:55 +0300

    changes in import react

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,5 @@
 import MarvelService from '../../services/MarvelService';
-import React, { Component } from 'react/cjs/react.development';
+import { Component } from 'react';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
