6dcc137fb5e74c002ce79eda186be6fe70b09e10
Gean <000cuiquesuum000@gmail.com>
2022-06-29 23:30:49 +0300

    Changed path to back from 404 page

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,24 +1,21 @@
 import ErrorMessage from "../errorMessage/ErrorMessage";
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 
 const Page404 = () => {
-    const history = useNavigate();
-
-    const updatePath = () => {
-        history(-1) ? history(-1) : history("/");
-    }
+    
     
     return (
         <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
             <ErrorMessage/>
             <p style={{textAlign: 'center', fontWeight: 'bold', fontSize: '24px'}}> This page not found </p>
-            <button 
-                onClick={updatePath} 
+            <Link 
+                to="/"
                 style={{cursor: 'pointer', marginTop: '30px', backgroundColor: 'lightblue',padding: '5px',  width: 'auto', textAlign: 'center', fontWeight: 'bold', fontSize: '24px'}}>
                 Go Back
                 {/* <Link style={{padding: '5px',  width: 'auto', textAlign: 'center', fontWeight: 'bold', fontSize: '24px'}} to="/">Back to main page</Link> */}
-            </button>
+            </Link>
         </div>
     )
 }
